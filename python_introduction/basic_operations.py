97a4ae3ee5cddbd368a8f162f256365d2a8c19d7
root <root@User-PC.localdomain>
2024-12-12 16:10:55 +0200

    Commiting the basic_operations.py script that Adds basic arithmetic operations with predefined numbers and readable output.

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,13 @@
+#Declaring and assigning my variables
+number1 = 10
+number2 = 5
+
+#Performing the Arethmatic operations
+Addition_value = number1 + number2
+Subtraction_value = number1 - number2
+Multiplication_value = number1 * number2
+
+#Printing out the output
+print("Addition of",number1,"and",number2,"is",Addition_value)
+print("Subtraction of",number1,"and",number2,"is",Subtraction_value)
+print("Multiplication of",number1,"and",number2,"is",Multiplication_value)
