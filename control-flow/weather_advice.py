7fe83461a5c275921ff088267facb81876fda39e
root <root@User-PC.localdomain>
2024-12-19 07:21:28 +0200

    Commiting the Weather recommandation script

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,18 +1,12 @@
 #Getting User for Weather Input
-current_weather = input("What's the weather like today? (sunny/rainy/cold): ")
-
-#Initializing clothing recommandation variable
-clothing_recommandation = ""
+weather = input("What's the weather like today? (sunny/rainy/cold): ")
 
 #Checking weather and recommending clothing
-if current_weather == "sunny":
-	clothing_recommandation = "Wear a t-shirt and sunglasses."
-elif current_weather == "rainy":
-	clothing_recommandation = "Don't forget your umbrella and a raincoat."
-elif current_weather == "cold":
-          clothing_recommandation = "Make sure to wear a warm coat and a scarf."
+if weather == "sunny":
+	print("Wear a t-shirt and sunglasses.")
+elif weather == "rainy":
+	print("Don't forget your umbrella and a raincoat.")
+elif weather == "cold":
+          print("Make sure to wear a warm coat and a scarf.")
 else:
-	clothing_recommandation = "Sorry, I don't have recommendations for this weather."
-
-#Printing the weather recommandation
-print(clothing_recommandation)
+	print("Sorry, I don't have recommendations for this weather.")
