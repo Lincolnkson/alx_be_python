492d7e0de30570a543c392f947762b6f7216ac4b
root <root@User-PC.localdomain>
2024-12-19 07:43:49 +0200

    committing match case

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,22 @@
+#Declearing number variables
+num1 = int(input("Enter the first number:"))
+num2 = int(input("Enter the second number:"))
+
+#Choosing math operator
+operator = input("Enter a math operator (+, -, *, /):")
+
+match operator:
+          case "+":
+                print(num1 + num2)
+          case "-":
+                print(num1 - num2)
+          case "*":
+              print(num1 * num2)
+          case "/":
+              if num2 != 0:
+                    print(num1 / num2)
+              else:
+                    print("Cannot divide by zero.")
+
+
+            
