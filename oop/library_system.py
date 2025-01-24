cf93e5db23fd601f2e96680290701e40ec34fb85
root <root@User-PC.localdomain>
2025-01-24 16:31:36 +0200

    Comitting 1. Mastering Inheritance and Composition in Python

--- oop/library_system.py
+++ oop/library_system.py
@@ -18,15 +18,6 @@ class PrintBook(Book):
                   self.page_count = page_count
           def __str__(self):
                   return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
-"""""
-Composition - Library:
-
-Attributes: books (a list to store instances of Book, EBook, and PrintBook).
-Methods:
-add_book(self, book): Adds a Book, EBook, or PrintBook instance to the library.
-list_books(self): Prints details of each book in the library.
-"""""
-
 
 class Library:
           def __init__(self):
@@ -35,20 +26,5 @@ class Library:
                   self.books.append(book)
           def list_books(self):
                   for book in self.books:
-                          print(book.title, book.author)
+                          print(book)
 
-"""""
-Pride and Prejudice Jane Austen
-Snow Crash Neal Stephenson
-The Catcher in the Rye J.D. Salinger
-
-(96 chars long)
-[stderr]: 
-(0 chars long)
-[Expected]
-
-Book: Pride and Prejudice by Jane Austen
-EBook: Snow Crash by Neal Stephenson, File Size: 500KB
-PrintBook: The Catcher in the Rye by J.D. Salinger, Page Count: 234
-
-"""""
